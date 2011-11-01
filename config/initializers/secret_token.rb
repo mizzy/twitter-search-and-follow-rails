20c6b9864f993730875697c232cf67cd05f1a2e5
Gosuke Miyashita <gosukenator@gmail.com>
2011-11-01 23:31:44 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TwitterSearchAndFollow::Application.config.secret_token = 'ebb3eda631259ac674ab8f5491b98b98ac17263f9fce751e12db7d397f19fe6844cd9832330e75a02cbcc968ce3f3a9cb07429d7f6ecd795aefd727e8b5cb2eb'
